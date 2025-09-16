965e2924a22e9a8d16f3d0aee948e7b7f2087375
AllScript <pnikita1109@gmail.com>
2025-09-16 09:18:06 +0300

    fix

--- first_lab/src/main/java/com/example/Palindrome.java
+++ first_lab/src/main/java/com/example/Palindrome.java
@@ -1,13 +1,19 @@
 package com.example;
 
 public class Palindrome {
-    public static boolean isPalindrome(String word) {
-        word = word.toLowerCase();
-        for (int i = 0; i < word.length() / 2; i++) {
-            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) { // Можно было использовать reverse, но так быстрее.
-                return false;
-            }
+
+    public static String reverseString(String text) {
+        String reversed = "";
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversed = reversed + text.charAt(i);
         }
-        return true;
+        return reversed;
+    }
+
+    public static boolean isPalindrome(String word) {
+        String original = word.toLowerCase();
+        String reversed = reverseString(original);
+
+        return original.equals(reversed);
     }
 }
